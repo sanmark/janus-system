2c6540aaf31868ca4cbfc3c87aa4f4bfb159fbb7
Supun Budhajeewa <budhajeewa@thesanmark.com>
2018-02-06 16:55:16 +0530

    Install "IDE Helper".

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers ;
 
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider ;
 use Illuminate\Support\ServiceProvider ;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +25,12 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function register ()
 	{
-		//
+		if ( $this -> app -> environment () !== 'production' )
+		{
+			$this
+				-> app
+				-> register ( IdeHelperServiceProvider::class ) ;
+		}
 	}
 
 }
